e57f40f4dd2d6fe09915dd33d87a8f601e4bcff8
mohdarsalanansari <mohdarsalan.ansari@coraltele.com>
2025-11-07 18:31:09 +0530

    added 1

--- spbsecurityinmemory/src/main/java/com/sde/spbsecurityinmemory/Configurations/InMemorySecurityConfig.java
+++ spbsecurityinmemory/src/main/java/com/sde/spbsecurityinmemory/Configurations/InMemorySecurityConfig.java
@@ -29,30 +29,18 @@ public class InMemorySecurityConfig {
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
 
         /*This function create the user object with fields/values  */
-        UserDetails user=User.withUsername("emp1").roles("user").password(passwordEncoder.encode("emp1_")).build();
+        UserDetails admin=User.withUsername("emp1").roles("admin").password(passwordEncoder.encode("emp1_")).build();
         
         /*This function created the user object empty then fills the details in it */
-        UserDetails manager=User.builder().username("emp2").roles("manager","user").password(passwordEncoder.encode("emp2_")).build();
+        UserDetails manager=User.builder().username("emp2").roles("manager").password(passwordEncoder.encode("emp2_")).build();
 
         /*this below function will first clone the manager user details then overwrite the existing fields like roles ,passswords ,username */
-        UserDetails admin=User.withUserDetails(manager).username("emp3").roles("admin","manager","user").password(passwordEncoder.encode("emp3_")).build();
+        UserDetails user=User.withUserDetails(manager).username("emp3").roles("user").password(passwordEncoder.encode("emp3_")).build();
         return new InMemoryUserDetailsManager(user,manager,admin);
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
-        // httpSecurity.authorizeHttpRequests(request->request.requestMatchers("/","/login","/css/**","/js/**").permitAll()
-        // .requestMatchers("/admin/**").hasRole("admin")
-        // .requestMatchers("/manager/**").hasAnyRole("admin","manager")
-        // .requestMatchers("/user/**").hasAnyRole("admin","manager","user")
-        // .anyRequest().authenticated()
-        // )
-        // .formLogin(form->form.loginPage("/login").defaultSuccessUrl("/",true).permitAll())
-        // .logout(form->form.logoutSuccessUrl("/login?logout").permitAll()
-        // );
-        // return httpSecurity.build();
-
-
 
         httpSecurity.authorizeHttpRequests(request->request.requestMatchers("/login","/css/**","/js/**").permitAll()
         .requestMatchers("/home","/service/**").hasAnyRole("user","admin","manager")
